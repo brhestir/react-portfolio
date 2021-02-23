8046c3846c3b1ddc6261eeaf583f0e66f5e21526
Brian Hestir <brhestir@gmail.com>
2021-02-23 00:23:43 -0500

    Added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,19 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <p>ima footer</p>
-    </div>
+    <>
+      <div class="footer">
+        <div class="container footer-top"></div>
+        <div class="container footer-bottom">
+          <address class="text-muted">
+            Copyright &copy; 2021{" "}
+            <a href="https://www.linkedin.com/in/brian-hestir-9743336">
+              Brian Hestir
+            </a>
+          </address>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,27 @@
 import React from 'react';
+import profilePicPng from '../assets/images/profilepic.jpeg';
 
 const Home = () => {
 	return (
 		<>
-			<h2>This is the home page</h2>
-			<br />
-
 			<div className="container-fluid h-100" id="container-background">
-            <div className="row h-100">
-                
-               
-                <div className="col-lg-8 col-sm-8">
-                    
-               
-                    <div className="card" id="card-content-box">
-                        <div className="card-body">
-                        	<h1 className="card-title">About Me</h1>
-													                          
-													<img src="./assets/images/profilepic.jpeg" className="rounded float-left w-25 mr-3" alt="Brian Hestir profile thumbnail"/>
-													<p className="card-text">My name is Brian Hestir.  I am a Software Development Engineer from Atlanta, GA.  I am currently enrolled in the Georgia Tech Full-Time Full-Stack Coding Bootcamp.</p>
-													<p className="card-text">I have a B.S. in Electrical Engineering from Clemson University.  I love learning new things, developing cool software and working on open-source tools.  On this site you can find recent projects I have worked on within the full-stack program.  Thanks for visiting!</p>
-													<p className="card-text">Check out my work at <a href="https://github.com/brhestir/">Github</a></p>
-													<p className="card-text">Connect with me on <a href="https://www.linkedin.com/in/brian-hestir-9743336/">LinkedIn</a></p>
-													<p className="card-text">Email: <a href="mailto:brhestir@gmail.com">brhestir@gmail.com</a></p>
-													<p className="card-text">Phone: 470-210-9202</p>
-													<a href="https://drive.google.com/file/d/1ksWjp3D2j3AgS3UXsbL9AMS7jhMZAzZd/view?usp=sharing">Resume: PDF</a>
-											</div>
-										</div>
-                </div>
-
-            </div>
+			<h2 className="text-center m-3">About Me</h2>
+				<div className="row h-100">
+					<div className="col-lg-8 col-sm-8">
+						<div className="card" id="card-content-box">
+							<div className="card-body">
+								<img src={profilePicPng} className="rounded float-left w-25 mr-3" alt="Brian Hestir profile thumbnail"/>
+								<p className="card-text">My name is Brian Hestir.  I am a Software Development Engineer from Atlanta, GA.  I am currently enrolled in the Georgia Tech Full-Time Full-Stack Coding Bootcamp.</p>
+								<p className="card-text">I have a B.S. in Electrical Engineering from Clemson University.  I love learning new things, developing cool software and working on open-source tools.  On this site you can find recent projects I have worked on within the full-stack program.  Thanks for visiting!</p>
+								<p className="card-text">Check out my work at <a href="https://github.com/brhestir/">Github</a></p>
+								<p className="card-text">Connect with me on <a href="https://www.linkedin.com/in/brian-hestir-9743336/">LinkedIn</a></p>
+								<p className="card-text">Email: <a href="mailto:brhestir@gmail.com">brhestir@gmail.com</a></p>
+								<a href="https://drive.google.com/file/d/1ksWjp3D2j3AgS3UXsbL9AMS7jhMZAzZd/view?usp=sharing">Resume: PDF</a>
+							</div>
+						</div>
 					</div>
-
+				</div>
+			</div>
 		</>
 	);
 };
