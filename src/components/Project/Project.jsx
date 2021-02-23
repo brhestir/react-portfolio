04463c751880e6baf452294070dfd2d074b468db
Brian Hestir <brhestir@gmail.com>
2021-02-23 00:00:20 -0500

    Updated projects page

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 
-const Project = () => {
+const Project = ({ title, image, desc, repoName, repo, site }) => {
   return (
-    <div>
-      <p>ima project</p>
-    </div>
+    <>
+      <div className="card">
+        <img
+          src={image}
+          className="card-img-top"
+          alt={repoName}
+          style={{ width: "400px", height: "300px" }}
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <h6 className="cart-subtitle">
+            <a href={site}>Deployed Site</a>
+          </h6>
+          <p className="card-text">{desc}</p>
+          <a href={repo} className="card-link">
+            {repoName} repository @ GitHub
+          </a>
+        </div>
+      </div>
+    </>
   );
 };
 
