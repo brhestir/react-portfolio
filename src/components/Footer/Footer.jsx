7d01214467bb172b100fa59a3f33ce7f20790aab
Brian Hestir <brhestir@gmail.com>
2021-04-12 14:05:47 -0400

    socialIcon ml-2, mr-2 and moved hrefs into const vars

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./footer.css";
 
+const link_email = "mailto:brhestir@gmail.com";
+const link_GitHub = "https://github.com/brhestir";
+const link_LinkedIn = "https://www.linkedin.com/in/brian-hestir-9743336";
+const link_resumePDF = "https://drive.google.com/file/d/1-GNCMynrWPrQnThQWwygVvBBUVlzUMRr/view?usp=sharing";
+
+
 const Footer = () => {
   return (
     <div>
@@ -8,10 +14,10 @@ const Footer = () => {
         <div className="container">
 				<address className="text-muted">
 						Copyright &copy; 2021{" "}Brian Hestir
-						<span className="socialIcon"> <a href="https://drive.google.com/file/d/1ksWjp3D2j3AgS3UXsbL9AMS7jhMZAzZd/view?usp=sharing" rel="noreferrer" target="_blank"><i className="fa fa-file-o fa-2x m-1" aria-label="Resume Icon"></i></a> </span>
-						<span className="socialIcon"> <a href="mailto:brhestir@gmail.com" rel="noreferrer" target="_blank"><i className="fa fa-envelope-o fa-2x m-1" aria-label="Email Icon"></i></a> </span>
-						<span className="socialIcon"> <a href="https://www.linkedin.com/in/brian-hestir-9743336" rel="noreferrer" target="_blank"><i className="fa fa-linkedin fa-2x m-1" aria-label="LinkedIn Icon"></i></a> </span>
-						<span className="socialIcon"> <a href="https://github.com/brhestir" rel="noreferrer" target="_blank"><i className="fa fa-github fa-2x m-1" aria-label="GitHub Icon"></i></a> </span>
+						<span className="socialIcon"> <a href={link_resumePDF} rel="noreferrer" target="_blank"><i className="fa fa-file-o fa-2x ml-2 mr-2" aria-label="Resume Icon"></i></a> </span>
+						<span className="socialIcon"> <a href={link_email} rel="noreferrer" target="_blank"><i className="fa fa-envelope-o fa-2x ml-2 mr-2" aria-label="Email Icon"></i></a> </span>
+						<span className="socialIcon"> <a href={link_LinkedIn} rel="noreferrer" target="_blank"><i className="fa fa-linkedin fa-2x ml-2 mr-2" aria-label="LinkedIn Icon"></i></a> </span>
+						<span className="socialIcon"> <a href={link_GitHub} rel="noreferrer" target="_blank"><i className="fa fa-github fa-2x ml-2 mr-2" aria-label="GitHub Icon"></i></a> </span>
 					</address>
 				</div>
       </div>
