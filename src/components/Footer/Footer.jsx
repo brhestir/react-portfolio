4d9ab4123be9b510a659079d9777d18da43956fe
Brian Hestir <brhestir@gmail.com>
2021-04-12 13:47:52 -0400

    In Footer component: Added footer.css: socialIcons -> float:right fa-2x m-1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./footer.css";
 
 const Footer = () => {
   return (
@@ -7,10 +8,10 @@ const Footer = () => {
         <div className="container">
 				<address className="text-muted">
 						Copyright &copy; 2021{" "}Brian Hestir
-						<span> <a href="https://github.com/brhestir" rel="noreferrer" target="_blank"><i className="fa fa-github fa-lg" aria-label="GitHub Icon"></i></a> </span>
-						<span> <a href="https://www.linkedin.com/in/brian-hestir-9743336" rel="noreferrer" target="_blank"><i className="fa fa-linkedin fa-lg" aria-label="LinkedIn Icon"></i></a> </span>
-						<span> <a href="mailto:brhestir@gmail.com" rel="noreferrer" target="_blank"><i className="fa fa-envelope-o fa-lg" aria-label="Email Icon"></i></a> </span>
-						<span> <a href="https://drive.google.com/file/d/1ksWjp3D2j3AgS3UXsbL9AMS7jhMZAzZd/view?usp=sharing" rel="noreferrer" target="_blank"><i className="fa fa-file-o fa-lg" aria-label="Resume Icon"></i></a> </span>
+						<span className="socialIcon"> <a href="https://drive.google.com/file/d/1ksWjp3D2j3AgS3UXsbL9AMS7jhMZAzZd/view?usp=sharing" rel="noreferrer" target="_blank"><i className="fa fa-file-o fa-2x m-1" aria-label="Resume Icon"></i></a> </span>
+						<span className="socialIcon"> <a href="mailto:brhestir@gmail.com" rel="noreferrer" target="_blank"><i className="fa fa-envelope-o fa-2x m-1" aria-label="Email Icon"></i></a> </span>
+						<span className="socialIcon"> <a href="https://www.linkedin.com/in/brian-hestir-9743336" rel="noreferrer" target="_blank"><i className="fa fa-linkedin fa-2x m-1" aria-label="LinkedIn Icon"></i></a> </span>
+						<span className="socialIcon"> <a href="https://github.com/brhestir" rel="noreferrer" target="_blank"><i className="fa fa-github fa-2x m-1" aria-label="GitHub Icon"></i></a> </span>
 					</address>
 				</div>
       </div>
